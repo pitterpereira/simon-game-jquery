2ddd1953f5d4b4107196263e52a099b248420ddd
Pitter Pereira da Costa <pitterpereira@gmail.com>
2023-01-16 15:40:55 -0300

    Melhorias e adição do recorde

--- scripts/game.js
+++ scripts/game.js
@@ -2,18 +2,20 @@
 /* VARIÁVEIS */
 /* ----------------------------------------------------------------------- */
 
-var coresDosBotoes = ["red", "blue", "green", "yellow"];
+let coresDosBotoes = ["red", "blue", "green", "yellow"];
 
 // Array que recebe as cores de cada etapa
-var padraoDoJogo = [];
-var padraoDoUsuario = [];
+let padraoDoJogo = [];
+let padraoDoUsuario = [];
 
-var level = 0;
+let level = 0;
+let recorde = 0;
 
 /* ----------------------------------------------------------------------- */
 /* LÓGICA */
 /* ----------------------------------------------------------------------- */
 
+// Iniciar o jogo
 $(document).keydown(function (event) {
 
     // Se teclar a letra "a", inicia o jogo
@@ -26,10 +28,11 @@ $(document).keydown(function (event) {
     }
 });
 
+// Em cada clique do mouse...
 $(".btn").click(function () {
 
     // Recebe a cor escolhida pelo clique do usuário e adiciona no array
-    var corClicada = $(this).attr('id');
+    let corClicada = $(this).attr('id');
     padraoDoUsuario.push(corClicada);
 
     animarClique(this);
@@ -52,8 +55,8 @@ function proximaSequencia() {
     $("#level-title").text("Level " + level);
 
     // Escolhe um número aleatório entre 0 e 3 e o coloca no padrão de acordo com a cor no vetor
-    var numeroAleat1a3 = Math.floor(Math.random() * 4);
-    var corAleatEscolhida = coresDosBotoes[numeroAleat1a3];
+    let numeroAleat1a3 = Math.floor(Math.random() * 4);
+    let corAleatEscolhida = coresDosBotoes[numeroAleat1a3];
     padraoDoJogo.push(corAleatEscolhida);
 
     // Pisca o botão e toca um som de acordo com a cor que saiu
@@ -67,6 +70,7 @@ function tocarSom(corClicada) {
     audio.play();
 }
 
+// Animação do clique no botão
 function animarClique(botao) {
     $(botao).addClass("pressed");
 
@@ -75,15 +79,18 @@ function animarClique(botao) {
     }, 100);
 }
 
+// Checa se a resposta do usuário está correta
 function checarResposta(levelAtual) {
-    
+
+    // Se o padrão de cliques for igual ao padrão proposto salvo até o momento...
     if (padraoDoJogo[levelAtual] === padraoDoUsuario[levelAtual]) {
         if (padraoDoJogo.length === padraoDoUsuario.length) {
             setTimeout(function () {
+                // Aguarda um segundo e propõe mais um item
                 proximaSequencia();
             }, 1000);
         }
-    } else {
+    } else { // Se errar o padrão...
         tocarSom("wrong");
         $("body").addClass("game-over");
         setTimeout(function () {
@@ -92,10 +99,17 @@ function checarResposta(levelAtual) {
 
         $("#level-title").text("Fim do jogo! Pressione 'A' para recomeçar");
 
+        // Seta o recorde no nível do último level alcançado, se for maior que o último recorde
+        if(level > recorde){
+            recorde = level;
+            $("h2 #recorde").text(recorde);
+        }            
+
         comecarDeNovo();
     }
 }
 
+// Reseta o nível e o padrão do jogo
 function comecarDeNovo() {
     level = 0;
     padraoDoJogo = [];
